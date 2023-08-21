613174c1eebaa0bee23cd35ea8b12daaeefa6771
Zhukov Kirill PC <zhukovk4010@gmail.com>
2023-08-21 16:00:03 +0300

    Добавить название карты и картинку с поездом

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import MapboxMap from './components/Mapbox-map'
+import trainPictures from './images/train-pictures.png'
 
 function App() {
 
 	console.log(process.env.REACT_APP_SECRET_CODE)
 	return (
 		<main className='mapContiner'>
+			<div className='map-name'>
+				<h1>ИСТОРИЯ РАЗВИТИЯ ЖЕЛЕЗНЫХ ДОРОГ АЗИАТСКОЙ ЧАСТИ РОССИИ</h1>
+			</div>
+			<div className='img-container'>
+				<img className='train-img' src={trainPictures} alt="" />
+			</div>
 			<MapboxMap />
 		</main>
 	);
